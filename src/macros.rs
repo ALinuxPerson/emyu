d93b54a941f9e11247474ac309449725316b416e
ALinuxPerson <alinuxperson@gmail.com>
2025-11-30 11:36:31 +0800

    Add message handler macro

--- src/macros.rs
+++ src/macros.rs
@@ -301,3 +301,57 @@ macro_rules! make_simple_model_getter {
         }
     };
 }
+
+#[macro_export]
+macro_rules! message_handler {
+    {
+        $(#[$meta:meta])*
+        $vis:vis struct $MsgName:ident {
+            $($fields:tt)*
+        } for $ModelName:ty;
+        $body:expr
+    } => {
+        $(#[$meta])*
+        $vis struct $MsgName {
+            $($fields)*
+        }
+        $crate:__message_handler_common!($MsgName $ModelName; $body);
+    };
+
+    {
+        $(#[$meta:meta])*
+        $vis:vis struct $MsgName:ident (
+            $($fields:tt)*
+        ) for $ModelName:ty;
+        $body:expr
+    } => {
+        $(#[$meta])*
+        $vis struct $MsgName ( $($fields)* );
+        $crate::__message_handler_common!($MsgName $ModelName; $body);
+    };
+
+    {
+        $(#[$meta:meta])*
+        $vis:vis struct $MsgName:ident for $ModelName:ty;
+        $body:expr
+    } => {
+        $(#[$meta])*
+        $vis struct $MsgName;
+        $crate::__message_handler_common!($MsgName $ModelName; $body);
+    };
+}
+
+#[macro_export]
+#[doc(hidden)]
+macro_rules! __message_handler_common {
+    ($MsgName:ident $ModelName:ty; $body:expr) => {
+        impl $crate::ModelMessage for $MsgName {}
+
+        impl $crate::ModelHandler<$MsgName> for $ModelName {
+            fn update(&mut self, message: $MsgName, ctx: &mut $crate::UpdateContext<<Self as $crate::Model>::ForApp>) {
+                let f: fn(&mut $ModelName, $MsgName, &mut $crate::UpdateContext<<Self as $crate::Model>::ForApp>) = $body;
+                f(self, message, ctx);
+            }
+        }
+    };
+}
