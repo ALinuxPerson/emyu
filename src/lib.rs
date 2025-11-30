7711a59035ce5a730b60dbe8d1c99d5c9c4666fc
ALinuxPerson <alinuxperson@gmail.com>
2025-11-30 12:29:20 +0800

    Add `command`, `message`, `getter` macros

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,11 @@
 #[macro_use]
 pub mod macros;
 
+#[doc(hidden)]
+pub mod __macros {
+    pub use async_trait::async_trait;
+}
+
 pub mod base;
 pub mod dispatcher;
 pub mod runtime;
--- src/macros.rs
+++ src/macros.rs
@@ -1,75 +1,158 @@
 #[macro_export]
-macro_rules! define_command {
+macro_rules! command {
+    ($($tt:tt)*) => {
+        $crate::__parse_struct!(
+            @impl $crate::__command_impl;
+            $($tt)*
+        );
+    };
+}
+
+#[macro_export]
+macro_rules! message {
+    ($($tt:tt)*) => {
+        $crate::__parse_struct!(
+            @impl $crate::__message_impl;
+            $($tt)*
+        );
+    };
+}
+
+#[macro_export]
+macro_rules! getter {
+    ($($tt:tt)*) => {
+        $crate::__parse_struct!(
+            @impl $crate::__getter_impl;
+            $($tt)*
+        );
+    };
+}
+
+#[macro_export]
+#[doc(hidden)]
+macro_rules! __parse_struct {
+    // 1. Named Fields: struct Name { ... }
     (
+        @impl $callback:path;
         $(#[$meta:meta])*
-        $name:ident($($type:ty),* $(,)?) for $app:ty;
-        fn($self:ident, $ctx:ident) $body:block
+        $vis:vis struct $Name:ident {
+            $($fields:tt)*
+        } $($rest:tt)*
     ) => {
         $(#[$meta])*
-        #[derive(Debug)]
-        pub struct $name($($type),*);
+        $vis struct $Name {
+            $($fields)*
+        }
+        $crate::__parse_header!(@impl $callback; $Name; $($rest)*);
+    };
 
-        #[::async_trait::async_trait]
-        impl $crate::Command for $name {
-            type ForApp = $app;
+    // 2. Tuple Struct: struct Name( ... );
+    (
+        @impl $callback:path;
+        $(#[$meta:meta])*
+        $vis:vis struct $Name:ident (
+            $($fields:tt)*
+        ) $($rest:tt)*
+    ) => {
+        $(#[$meta])*
+        $vis struct $Name ( $($fields)* );
+        $crate::__parse_header!(@impl $callback; $Name; $($rest)*);
+    };
 
-            async fn apply(&mut self, $ctx: &mut ExecContext<'_, Self::ForApp>) {
-                let $self  = self;
-                $body
+    // 3. Unit Struct: struct Name;
+    (
+        @impl $callback:path;
+        $(#[$meta:meta])*
+        $vis:vis struct $Name:ident $($rest:tt)*
+    ) => {
+        $(#[$meta])*
+        $vis struct $Name;
+        $crate::__parse_header!(@impl $callback; $Name; $($rest)*);
+    };
+}
+
+#[macro_export]
+#[doc(hidden)]
+macro_rules! __parse_header {
+    // hacky workaround for `__getter_impl`, making this macro not really generic anymore is it
+    (
+        @impl $callback:path;
+        $Name:ident;
+        for $Model:ty where Data = $Ret:ty;
+        $($rest:tt)*
+    ) => {
+        $callback!($Name $Model; where Data = $Ret; $($rest)*);
+    };
+    (
+        @impl $callback:path;
+        $Name:ident;
+        for $Model:ty;
+        $($rest:tt)*
+    ) => {
+        $callback!($Name $Model; ; $($rest)*);
+    };
+}
+
+
+#[macro_export]
+#[doc(hidden)]
+macro_rules! __command_impl {
+    (
+        $CommandName:ident $ModelName:ty;
+        ;
+        | $this:ident, $ctx:ident | $body:expr
+    ) => {
+        #[$crate::__macros::async_trait]
+        impl $crate::Command for $CommandName {
+            type ForApp = <$ModelName as $crate::Model>::ForApp;
+
+            async fn apply(&mut self, ctx: &mut $crate::ApplyContext<'_, Self::ForApp>) {
+                async fn f(
+                    $this: &mut $CommandName,
+                    $ctx: &mut $crate::ApplyContext<'_, <$CommandName as $crate::Command>::ForApp>,
+                ) {
+                    $body
+                }
+                f(self, ctx).await
             }
         }
     };
 }
 
 #[macro_export]
-macro_rules! message_handler {
-    {
-        $(#[$meta:meta])*
-        $vis:vis struct $MsgName:ident {
-            $($fields:tt)*
-        } for $ModelName:ty;
-        $body:expr
-    } => {
-        $(#[$meta])*
-        $vis struct $MsgName {
-            $($fields)*
-        }
-        $crate:__message_handler_common!($MsgName $ModelName; $body);
-    };
-
-    {
-        $(#[$meta:meta])*
-        $vis:vis struct $MsgName:ident (
-            $($fields:tt)*
-        ) for $ModelName:ty;
-        $body:expr
-    } => {
-        $(#[$meta])*
-        $vis struct $MsgName ( $($fields)* );
-        $crate::__message_handler_common!($MsgName $ModelName; $body);
-    };
-
-    {
-        $(#[$meta:meta])*
-        $vis:vis struct $MsgName:ident for $ModelName:ty;
-        $body:expr
-    } => {
-        $(#[$meta])*
-        $vis struct $MsgName;
-        $crate::__message_handler_common!($MsgName $ModelName; $body);
-    };
-}
-
-#[macro_export]
 #[doc(hidden)]
-macro_rules! __message_handler_common {
-    ($MsgName:ident $ModelName:ty; $body:expr) => {
+macro_rules! __message_impl {
+    (
+        $MsgName:ident $ModelName:ty;
+        $body:expr
+    ) => {
         impl $crate::ModelMessage for $MsgName {}
 
         impl $crate::ModelHandler<$MsgName> for $ModelName {
             fn update(&mut self, message: $MsgName, ctx: &mut $crate::UpdateContext<<Self as $crate::Model>::ForApp>) {
                 let f: fn(&mut $ModelName, $MsgName, &mut $crate::UpdateContext<<Self as $crate::Model>::ForApp>) = $body;
                 f(self, message, ctx);
+            }
+        }
+    };
+}
+
+#[macro_export]
+#[doc(hidden)]
+macro_rules! __getter_impl {
+    (
+        $MsgName:ident $ModelName:ty;
+        where Data = $Ret:ty;
+        | $this:ident, $msg:ident | $body:expr
+    ) => {
+        impl $crate::ModelGetterMessage for $MsgName {
+            type Data = $Ret;
+        }
+
+        impl $crate::ModelGetterHandler<$MsgName> for $ModelName {
+            fn getter(&self, message: $MsgName) -> $Ret {
+                let f: fn(&$ModelName, $MsgName) -> $Ret = | $this, $msg | $body;
+                f(self, message)
             }
         }
     };
