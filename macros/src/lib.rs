e276b7dc5fbb1fd4559a998dadd6d9552c25a68f
ALinuxPerson <alinuxperson@gmail.com>
2025-12-05 11:33:27 +0800

    Remove `dispatcher` macro

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,4 +1,3 @@
-mod dispatcher;
 mod command;
 mod model;
 mod utils;
@@ -15,16 +14,6 @@ fn crate_() -> proc_macro2::TokenStream {
             let ident = Ident::new(&name, Span::call_site());
             quote! { #ident }
         }
-    }
-}
-
-#[proc_macro_attribute]
-pub fn dispatcher(args: TokenStream, input: TokenStream) -> TokenStream {
-    let args = syn::parse_macro_input!(args as dispatcher::DispatcherArgs);
-    let input = syn::parse_macro_input!(input as utils::InterfaceImpl);
-    match dispatcher::build(input, args) {
-        Ok(tokens) => tokens.into(),
-        Err(err) => err.to_compile_error().into(),
     }
 }
 
