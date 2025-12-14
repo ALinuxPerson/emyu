cb01f72508cc80de21438ab772ceb55ad037bdcd
ALinuxPerson <alinuxperson@gmail.com>
2025-12-14 14:19:27 +0800

    Remove `command` proc macro

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,18 +1,7 @@
-mod command;
 mod model;
 mod utils;
 
 use proc_macro::TokenStream;
-
-#[proc_macro_attribute]
-pub fn command(args: TokenStream, input: TokenStream) -> TokenStream {
-    let args = syn::parse_macro_input!(args as command::CommandArgs);
-    let input = syn::parse_macro_input!(input as syn::ItemFn);
-    match command::build(input, args) {
-        Ok(tokens) => tokens.into(),
-        Err(err) => err.to_compile_error().into(),
-    }
-}
 
 #[proc_macro_attribute]
 pub fn model(args: TokenStream, input: TokenStream) -> TokenStream {
